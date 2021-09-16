28ea6d69ec87567c27e615a2c03e520116aa10fe
langej11 <langej11@gmatc.matc.edu>
2021-09-15 22:30:21 -0500

    added database connection

--- Assignment2/server.js
+++ Assignment2/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import connectDatabase from './config/db';
 
 //initalize express application
 const app = express();
 
+
+//connect database
+connectDatabase();
 //api end points
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
