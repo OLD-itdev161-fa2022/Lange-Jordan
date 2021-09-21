6f6b3468a378f4dc4a2b9fa04e7d3aca6ef345ab
langej11 <langej11@gmatc.matc.edu>
2021-09-20 23:27:30 -0500

    created user registration api

--- Assignment3/server.js
+++ Assignment3/server.js
@@ -7,10 +7,27 @@ const app = express();
 
 //connect database
 connectDatabase();
+
+//configure middleware
+app.use(express.json({ extended:: false}));
+
 //api end points
+/**
+ * @route GET
+ * @desc test endpoint
+ */
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+/**
+ * @route POST api/users
+ * @desc register user
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);;
+});
 
 //connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
